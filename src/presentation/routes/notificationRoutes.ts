a3c8defd67b4a535b10d0a19355c0280f8ac645a
AnanthuTD <ananthutd2021@gmail.com>
2025-04-22 02:04:05 +0530

    update

--- src/presentation/routes/notificationRoutes.ts
+++ src/presentation/routes/notificationRoutes.ts
@@ -3,7 +3,7 @@ import env from "@/infra/env";
 
 export const notificationRoutes = (): RouteConfig[] => [
 	createRoute("/notifications", true, {
-		target: env.NOTIFICATION_SERVICE_API,
+		target: env.NOTIFICATION_SERVICE_API + '/api',
 		changeOrigin: true,
 	}),
 ];
--- src/presentation/routes/videoRoutes.ts
+++ src/presentation/routes/videoRoutes.ts
@@ -2,29 +2,34 @@ import { createRoute, RouteConfig } from ".";
 import env from "@/infra/env";
 
 export const videoRoutes = (): RouteConfig[] => [
-  createRoute(
-    "/videos/ws",
-    false,
-    {
-      target: env.COLLABORATION_SERVICE_API,
-      changeOrigin: true,
-      pathRewrite: { "^/": "/socket.io/" },
-      ws: true,
-    },
-    null,
-    null,
-    "admin"
-  ),
-  createRoute("/videos/:videoId/preview", false, {
-    target: env.VIDEO_SERVICE_API,
-    changeOrigin: true,
-    pathRewrite: function (path, req) {
-      return req.originalUrl.replace(/^\/videos/, '');
-    },
-  }),
-  createRoute("/videos", true, {
-    target: env.VIDEO_SERVICE_API,
-    changeOrigin: true,
-    pathRewrite: { "^/": "/api/" },
-  }),
+	createRoute(
+		"/videos/ws",
+		false,
+		{
+			target: env.COLLABORATION_SERVICE_API,
+			changeOrigin: true,
+			pathRewrite: { "^/": "/socket.io/" },
+			ws: true,
+		},
+		null,
+		null,
+		"admin"
+	),
+	createRoute("/videos/:videoId/preview", false, {
+		target: env.VIDEO_SERVICE_API,
+		changeOrigin: true,
+		pathRewrite: function (path, req) {
+			return req.originalUrl.replace(/^\/videos/, "");
+		},
+	}),
+	createRoute("/videos/public", false, {
+		target: env.VIDEO_SERVICE_API,
+		changeOrigin: true,
+		pathRewrite: { "^/": "/api/public/" },
+	}),
+	createRoute("/videos", true, {
+		target: env.VIDEO_SERVICE_API,
+		changeOrigin: true,
+		pathRewrite: { "^/": "/api/" },
+	}),
 ];
